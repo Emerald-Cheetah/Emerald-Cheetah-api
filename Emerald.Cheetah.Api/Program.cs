882525bb9ac231a44be5a6c08441347d7d020f90
El Hadji Diouf <chimerediouf2000@gmail.com>
2026-03-30 23:02:40 -0400

    add Id to Rating and update gitignore for sqlite

--- Emerald.Cheetah.Api/Program.cs
+++ Emerald.Cheetah.Api/Program.cs
@@ -1,6 +1,14 @@
+using Emerald.Cheetah.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite("Data Source=./Registrar.sqlite",
+        b => b.MigrationsAssembly("Emerald.Cheetah.Api")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -11,7 +19,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.MapControllers();
 
