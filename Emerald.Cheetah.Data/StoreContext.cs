e496c85a3076177062b5b3211ca140a3f4bf36cf
El Hadji Diouf <chimerediouf2000@gmail.com>
2026-03-30 21:55:30 -0400

    add StoreContext for EF core data acess

---
+++ Emerald.Cheetah.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Emerald.Cheetah.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emerald.Cheetah.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
